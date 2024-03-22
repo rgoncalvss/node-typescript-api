1480a85004d33dda8a465a6225b3da437fcea406
rgoncalvss <carolinor7@gmail.com>
2024-03-21 22:48:47 -0300

    refact

--- src/controllers/books.controller.ts
+++ src/controllers/books.controller.ts
@@ -1,99 +1,107 @@
-import { NextFunction, Request, Response } from "express";
-import { Book } from "../models/Book";
-import { BadRequestException } from "../exceptions/bad-request";
-import { ErrorCode } from "../exceptions/root";
-import { NotFoundException } from "../exceptions/not-found";
-import { User } from "../models/User";
+import { NextFunction, Request, Response } from 'express';
+import { BadRequestException } from '../exceptions/bad-request';
+import { ErrorCode } from '../exceptions/root';
+import { NotFoundException } from '../exceptions/not-found';
+import booksRepository from '../repositories/BooksRepository';
+import usersRepository from '../repositories/UsersRepository';
 
-export class BookController {
-  static async create(req: Request, res: Response, next: NextFunction) {
+class BookController {
+  async create(req: Request, res: Response, next: NextFunction) {
     const { title, available } = req.body;
 
     if (!title || !available) {
       next(
         new BadRequestException(
-          "Title and book availability are required",
+          'Title and book availability are required',
           ErrorCode.BAD_REQUEST,
         ),
       );
       return;
     }
 
-    const bookAlreadyExists = await Book.findByTitle(req.body.title);
+    const bookAlreadyExists = await booksRepository.findByTitle(req.body.title);
 
     if (bookAlreadyExists) {
-      next(new BadRequestException("Book already exists", ErrorCode.CONFLICT));
+      next(new BadRequestException('Book already exists', ErrorCode.CONFLICT));
       return;
     }
 
-    const book = await Book.create(req.body);
+    const book = await booksRepository.create(req.body);
 
     res.json(book);
   }
 
-  static async lend(req: Request, res: Response, next: NextFunction) {
+  async getAll(req: Request, res: Response) {
+    const books = await booksRepository.getAll();
+    res.json(books);
+  }
+
+  async lend(req: Request, res: Response, next: NextFunction) {
     const bookId = parseInt(req.params.id, 10);
     const userId = parseInt(req.body.userId, 10);
 
     if (!userId || !bookId) {
       next(
         new BadRequestException(
-          "Book and User Id are required",
+          'Book and User Id are required',
           ErrorCode.BAD_REQUEST,
         ),
       );
       return;
     }
 
-    const book = await Book.findById(bookId);
-    const user = await User.findById(userId);
+    const book = await booksRepository.findById(bookId);
+    const user = await usersRepository.findById(userId);
 
     if (!user) {
-      next(new NotFoundException("User not found", ErrorCode.NOT_FOUND));
+      next(new NotFoundException('User not found', ErrorCode.NOT_FOUND));
       return;
     }
 
     if (!book) {
-      next(new NotFoundException("Book not found", ErrorCode.NOT_FOUND));
+      next(new NotFoundException('Book not found', ErrorCode.NOT_FOUND));
       return;
     }
 
     if (!book.available) {
-      next(new BadRequestException("Book already lent", ErrorCode.CONFLICT));
+      next(new BadRequestException('Book already lent', ErrorCode.CONFLICT));
       return;
     }
 
-    await Book.lend(bookId, userId);
+    await booksRepository.lend(bookId, userId);
 
-    res.json({ msg: "Book lent successfully!" });
+    res.json({ msg: 'Book lent successfully!' });
   }
 
-  static async return(req: Request, res: Response, next: NextFunction) {
+  async return(req: Request, res: Response, next: NextFunction) {
     const bookId = parseInt(req.params.id, 10);
 
     if (!bookId) {
       next(
-        new BadRequestException("Book Id is required", ErrorCode.BAD_REQUEST),
+        new BadRequestException('Book Id is required', ErrorCode.BAD_REQUEST),
       );
       return;
     }
 
-    const book = await Book.findById(bookId);
+    const book = await booksRepository.findById(bookId);
 
     if (!book) {
-      next(new NotFoundException("Book not found", ErrorCode.NOT_FOUND));
+      next(new NotFoundException('Book not found', ErrorCode.NOT_FOUND));
       return;
     }
 
     if (book.available) {
       next(
-        new BadRequestException("Book already returned", ErrorCode.CONFLICT),
+        new BadRequestException('Book already returned', ErrorCode.CONFLICT),
       );
       return;
     }
 
-    await Book.return(bookId);
+    await booksRepository.return(bookId);
 
-    res.json({ msg: "Book returned successfully!" });
+    res.json({ msg: 'Book returned successfully!' });
   }
 }
+
+// Singleton pattern
+export default new BookController();
--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,34 +1,36 @@
-import { NextFunction, Request, Response } from "express";
-import { User } from "../models/User";
-import { BadRequestException } from "../exceptions/bad-request";
-import { ErrorCode } from "../exceptions/root";
-import { NotFoundException } from "../exceptions/not-found";
-
-export class UserController {
-  static async create(req: Request, res: Response, next: NextFunction) {
+import { NextFunction, Request, Response } from 'express';
+import { BadRequestException } from '../exceptions/bad-request';
+import { ErrorCode } from '../exceptions/root';
+import { NotFoundException } from '../exceptions/not-found';
+import usersRepository from '../repositories/UsersRepository';
+class UserController {
+  async create(req: Request, res: Response, next: NextFunction) {
     const { email, name } = req.body;
 
-    const userAlreadyExists = await User.findByEmail(email);
+    const userAlreadyExists = await usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
-      next(new BadRequestException("User already exists", ErrorCode.CONFLICT));
+      next(new BadRequestException('User already exists', ErrorCode.CONFLICT));
       return;
     }
 
-    const user = await User.create(email, name);
+    const user = await usersRepository.create(email, name);
 
     res.json(user);
   }
 
-  static async findById(req: Request, res: Response, next: NextFunction) {
+  async findById(req: Request, res: Response, next: NextFunction) {
     const userId = parseInt(req.params.id, 10);
-    const user = await User.findById(userId);
+    const user = await usersRepository.findById(userId);
 
     if (!user) {
-      next(new NotFoundException("User not found", ErrorCode.NOT_FOUND));
+      next(new NotFoundException('User not found', ErrorCode.NOT_FOUND));
       return;
     }
 
     return res.json(user);
   }
 }
+
+//Singleton pattern
+export default new UserController();
